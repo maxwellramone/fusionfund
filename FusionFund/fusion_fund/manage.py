31c5d72120c2a76db25e98c1309864df5117f5c0
maxwellramone <maxwellramone@gmail.com>
2023-09-28 21:57:27 -0400

    Initial commit

---
+++ FusionFund/fusion_fund/manage.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fusion_fund.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+def getTitle(url):
+	import requests
+	from bs4 import BeautifulSoup
+
+	url = 'https://www.reuters.com/legal/appeals-court-will-not-delay-donald-trump-civil-fraud-trial-2023-09-28/'
+
+	try:
+		response = requests.get(url)
+		response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
+
+		if response.status_code == 200:
+			soup = BeautifulSoup(response.text, 'html.parser')
+			
+			# article_titles = soup.find_all('title', class_='html.parser')  # Use a dictionary for attributes
+			article_titles = soup.find_all('p')
+			for title in article_titles:
+				print(title.text)
+		else:
+			print('Failed. Code:', response.status_code)
+
+	except requests.exceptions.RequestException as e:
+		print('Request Exception:', e)
+
+	except Exception as e:
+		print('An error occurred:', e)
